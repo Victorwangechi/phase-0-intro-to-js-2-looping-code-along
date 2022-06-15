43e15789ccdf7d0e4a26e01ebf38ee1b238c2ab0
Victor Wangechi <murkyvictor@gmail.com>
2022-06-15 10:41:29 +0300

    edited

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(){
+
+}
+
+  function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+
+      console.log( startingNumber );
+
+      startingNumber -= 1;
+    }
+    console.log( startingNumber);
+  }
+
